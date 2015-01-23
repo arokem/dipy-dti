a0704d747c1589a3d9dd3e02e74318bb2f71cdd5
arokem <arokem@gmail.com>
2015-01-23 10:56:26 -0800

    Streamline.

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,5 @@
 FROM arokem/dipy
 
-RUN apt-get update
-RUN apt-get install -y ipython ipython-notebook
-RUN luarocks install itorch
-
-ADD /input /input
-ADD /output /output
-WORKDIR /workspace
 ADD dti.py /dti.py
 RUN chmod a+x /dti.py
 
--- dti.py
+++ dti.py
@@ -7,8 +7,9 @@ import dipy.reconst.dti as dti
 from dipy.io.utils import nifti1_symmat
 import dipy.core.gradients as grad
 
-def dipysave(img, filename):
-    """Some DTI/tools require the qform code to be 1. We set the affine, qform,
+def dpsave(img, filename):
+    """
+    Some tools require the qform code to be 1. We set the affine, qform,
     and sfrom to be the same for maximum portability.
     """
     affine = img.get_affine()
@@ -17,46 +18,36 @@ def dipysave(img, filename):
     nib.save(img, filename)
 
 
-#fmetadata = "/input/metadata.json"
-fmetadata = "/Users/arokem/tmp/metadata.json"
+if __name__="__main__":
+    fmetadata = "/input/metadata.json"
+    # Fetch metadata:
+    metadata = json.load(open(fmetadata))
+    fdata, fbval, fbvec = [metadata[k] for k in ["fdata", "fbval", "fbvec"]]
 
-# Fetch metadata:
-metadata = json.load(open(fmetadata))
-fdata, fbval, fbvec = [metadata[k] for k in ["fdata", "fbval", "fbvec"]]
+    # Load the data:
+    img = nib.load(str(fdata))
+    gtab = grad.gradient_table(str(fbval), str(fbvec))
+    data = img.get_data()
+    affine = img.get_affine()
 
-# Load the data:
-img = nib.load(str(fdata))
-gtab = grad.gradient_table(str(fbval), str(fbvec))
-data = img.get_data()
-affine = img.get_affine()
+    # Fit the model:
+    tenmodel = dti.TensorModel(gtab)
+    tenfit = tenmodel.fit(data)
 
-# Fit the model:
-tenmodel = dti.TensorModel(gtab)
-tenfit = tenmodel.fit(data)
+    # Extract the nifti convention parameters:
+    lower_triangular = tenfit.lower_triangular()
+    lower_triangular = lower_triangular.astype('float32')
+    tensor_img = nifti1_symmat(lower_triangular, affine)
 
-# Extract the nifti convention parameters:
-lower_triangular = tenfit.lower_triangular()
-lower_triangular = lower_triangular.astype('float32')
-tensor_img = nifti1_symmat(lower_triangular, affine)
+    # The output will all have the same basic name as the data file-name:
+    root = op.join("/output",
+                   op.splitext(op.splitext(op.split(fdata)[-1])[0])[0])
+    
+    # Save to file:
+    dpsave(tensor_img, root + '_tensor.nii.gz')
+    for arr, label in zip([tenfit.ad, tenfit.rd, tenfit.md, tenfit.fa],
+                          ["ad", "rd", "md", "fa"]):
+        dpsave(nib.Nifti1Image(arr.astype("float32"), affine),
+               op.join('/output/', root + '%.nii.gz'%label)
 
-root = op.join("/output/")
-
-# Save to file:
-dipysave(tensor_img, root+'_tensor.nii.gz')
-dipysave(nib.Nifti1Image(ten.ad.astype("float32"), affine), root+'_ad.nii.gz')
-dipysave(nib.Nifti1Image(ten.rd.astype("float32"), affine), root+'_rd.nii.gz')
-dipysave(nib.Nifti1Image(ten.md.astype("float32"), affine), root+'_md.nii.gz')
-dipysave(nib.Nifti1Image(ten.fa.astype("float32"), affine), root+'_fa.nii.gz')
-
-
-FA = fractional_anisotropy(tenfit.evals)
-FA[np.isnan(FA)] = 0
-fa_img = nib.Nifti1Image(FA.astype(np.float32), img.get_affine())
-nib.save(fa_img, 'tensor_fa.nii.gz')
-evecs_img = nib.Nifti1Image(tenfit.evecs.astype(np.float32),
-                            img.get_affine())
-nib.save(evecs_img, 'tensor_evecs.nii.gz')
-MD = tenfit.md
-nib.save(nib.Nifti1Image(MD.astype(np.float32),
-                         img.get_affine()), 'tensors_md.nii.gz')
 
